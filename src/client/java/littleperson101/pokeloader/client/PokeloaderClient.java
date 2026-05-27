602af0b8c9c6b5c6018110f2d894cd10ed4a6d2b
LittlePerson101 <hostilecreeper10@gmail.com>
2026-05-27 12:07:42 -0500

    Initial commit

---
+++ src/client/java/littleperson101/pokeloader/client/PokeloaderClient.java
@@ -0,0 +1,10 @@
+package littleperson101.pokeloader.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PokeloaderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
